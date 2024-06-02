ed0053e73d8cd1f4d5bc7a48baee39344707541a
Arthur Guilherme Ribeiro Duarte <117129364+ArthurrGuilherme@users.noreply.github.com>
2024-06-01 23:02:57 -0300

    Add files via upload

--- js/formulario.js
+++ js/formulario.js
@@ -13,3 +13,21 @@ function validateForm() {
 
     return true;
 }
+
+var form = document.getElementById('sheetdb-form');
+    form.addEventListener("submit", e => {
+      e.preventDefault();
+      fetch(form.action, {
+          method : "POST",
+          body: new FormData(document.getElementById("sheetdb-form")),
+      }).then(
+          response => response.json()
+      ).then((html) => {
+        // you can put any JS code here
+        swal({
+            title: "Sucesso",
+            text: "Seu cadastro foi enviado",
+            icon: "success",
+        })
+      });
+    });
